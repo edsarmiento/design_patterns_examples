add51311568e2ff489e710b338b9254ed70b99e3
David ST <edst83@gmail.com>
2024-07-29 16:10:16 -0600

    add inject method

---
+++ common_algorithms/inject_method.rb
@@ -0,0 +1,19 @@
+# inject numbers
+arr = [1,3,4,5,6,7,9]
+result = arr.inject(0) do |accum, num|
+  accum + num
+end
+
+puts "both are same result"
+print result
+puts "\n"
+puts arr.sum
+
+# inject strings
+arr = ['hola', 'mundo', 'how are', 'you']
+result = arr.inject('') do |accum, num|
+  accum + num + ' '
+end
+
+print result
+puts "\n"
